4e6c0fc9116ce5bd9449dee6bedca24872395986
MichaelHochriegl <michael@hochriegl.dev>
2022-12-07 16:44:36 +0100

    add small sample

---
+++ samples/ConsoleSample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Thready.Lib;
+
+var count = 0;
+
+Console.WriteLine("Hello, Thready!");
+var worker = new ThreadyWorker();
+
+await worker.StartAsync(IncreaseCount, TimeSpan.FromSeconds(1));
+
+Console.ReadKey();
+await worker.StopAsync();
+
+async Task IncreaseCount()
+{
+    count++;
+    Console.WriteLine(count);
+}
